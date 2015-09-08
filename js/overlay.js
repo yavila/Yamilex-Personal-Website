144da14d355023f86647eb4eae4c35bd5103629b
Yamilex Avila-Stanley <yjas3@gatech.edu>
2015-09-07 20:33:29 -0400

    initial commit

---
+++ js/overlay.js
@@ -0,0 +1,52 @@
+(function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' ),
+		homeBttn = document.getElementById('home-bttn'),
+		aboutBttn = document.getElementById('about-me-bttn'),
+		projectBttn = document.getElementById('projects-bttn'),
+		contactBttn = document.getElementById('contact-bttn'),
+		body = document.getElementById('body'),
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			// Disable scroll while open
+			// body.style.overflow = 'visible';
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			// body.style.overflow = 'hidden';
+			classie.add( overlay, 'open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+	homeBttn.addEventListener('click', toggleOverlay);
+	aboutBttn.addEventListener( 'click', toggleOverlay );
+	projectBttn.addEventListener( 'click', toggleOverlay);
+	contactBttn.addEventListener( 'click', toggleOverlay);
+})();
